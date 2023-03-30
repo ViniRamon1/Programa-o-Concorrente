f75dd252716b894af134394fabd37a2916efaea5
Vinicius Ramon <93198197+ViniRamon1@users.noreply.github.com>
2023-03-30 16:46:06 -0300

    corrigindo bugs

--- aproximacaopi.c
+++ aproximacaopi.c
@@ -6,43 +6,43 @@
 #define num_passos 300000000
 
 int main() {
-    double step;
-    int i; double x, pi, soma = 0.0;
+
+    double passo;
+    int i; double x, pi, sum = 0.0;
     int t = omp_get_num_threads();
 
-    printf("Theads: %d\n",t);
     double inicio = omp_get_wtime();
-    step = 1.0/(double) num_passos;
+    passo = 1.0/(double) num_passos;
     for (i=0;i< num_passos; i++){
-        x = (i + 0.5) * step; 
-        soma = soma + 4.0 / (1.0 + x*x); 
+        x = (i + 0.5) * passo; 
+        sum = sum + 4.0 / (1.0 + x*x); 
     }
-    pi = step * soma;
+    pi = passo * sum;
     double fim = omp_get_wtime();
-    double t_serial = fim-inicio;
-    printf("%.5f\n",pi);
-    printf("Execucao sequencial(s): %f\n",t_serial);
+    double temposeq = fim-inicio;
+    printf("%f\n",pi);
+    printf("Execucao sequencial: %f\n",temposeq);
 
     x = 0;
     pi = 0;
-    soma = 0;
+    sum = 0;
     inicio = omp_get_wtime();
-    step = 1.0/(double) num_passos;
-    #pragma omp parallel private(x) num_threads(4)
+    passo = 1.0/(double)num_passos;
+    #pragma omp parallel private(x) num_threads(3)
     {
-        #pragma omp for reduction (+:soma)
-        for (i=0;i< num_passos; i++){
-            x = (i + 0.5) * step; 
-            soma = soma + 4.0 / (1.0 + x*x); 
+        #pragma omp for reduction (+:sum)
+        for (i=0; i< num_passos; i++){
+            x = (i + 0.5) * passo; 
+            sum = sum + 4.0 / (1.0 + x*x); 
+        }
     }
-    pi = step * soma;
+    pi = passo * sum;
     fim = omp_get_wtime();
-    double t_paralelo = fim-inicio;
-    double speedup = t_serial/t_paralelo;
+    double tempoparalelo = fim-inicio;
+    double speedup = temposeq/tempoparalelo;
     printf("Pi: %f\n",pi);
-    printf("Execucao paralela: %f\n",t_paralelo);
-    printf("Speedup: %f\n", speedup);
-	printf("Eficiencia: %f\n",speedup/4.0);
+    printf("Execucao paralela: %f\n",tempoparalelo);
+    printf("Speedup: %.4f\n", speedup);
+    printf("Eficiencia: %.4f\n",speedup/3.0);
     return 0;
-    }
 }
